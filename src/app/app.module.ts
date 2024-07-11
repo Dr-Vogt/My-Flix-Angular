e2600334be812bd885b34b04fe6b46ba67d863d0
Dr-Vogt <145719602+Dr-Vogt@users.noreply.github.com>
2024-07-11 12:52:10 -0700

    fixxed login button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component'
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     MovieCardComponent,
-    UserLoginFormComponent
+    UserLoginFormComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -11,35 +11,32 @@ import { UserRegistrationFormComponent } from '../user-registration-form/user-re
   styleUrls: ['./user-login-form.component.scss']
 })
   export class UserLoginFormComponent implements OnInit {
-    @Input() userData = { username: "", password: "" };
+    @Input() userData = {
+       username: "",
+       password: "",
+       };
     constructor(
         public fetchApiData: FetchApiDataService,
         public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
         public snackBar: MatSnackBar,
-        public router: Router
     ) { }
 
     ngOnInit(): void {}
-    logInUser() : void {
-        this.fetchApiData.userLogin(this.userData).subscribe(res => {
-            this.dialogRef.close();
-            this.snackBar.open(`Login success, Welcom ${res.user.username}`, "OK", {
-                duration: 2000
-            });
-            let user = {
-                id: res.user._id,
-                username: res.user.Username,
-                birthday: res.user.Bbirthday,
-                email: res.user.Email,
-                token: res.token
-            }
-            
-            localStorage.setItem("user", JSON.stringify(user));
-            this.router.navigate(["movies"]);
-        }, res => {
-            this.snackBar.open("Login fail", "OK", {
-                duration: 2000
-            })
-        })
+    loginUser(): void {
+      this.fetchApiData.userLogin(this.userData).subscribe(
+        (response) => {
+          this.dialogRef.close();
+          this.snackBar.open(response, 'OK', {
+            duration: 2000,
+          });
+          localStorage.setItem('currentUser', JSON.stringify(response.user));
+          localStorage.setItem('token', response.token);
+        },
+        (response) => {
+          this.snackBar.open(response, 'NOT OK', {
+            duration: 9000,
+          });
+        }
+      );
     }
-}
+  }
